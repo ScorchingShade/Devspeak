46babcb83e88c76e60aececffceb7beecf6b21f2
ScorchingShade <ankushors789@gmail.com>
2021-10-08 19:00:19 +0530

    added push for the routing if already logged in

--- src/index.js
+++ src/index.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "semantic-ui-css/semantic.min.css"
 import {BrowserRouter as Router, Switch,Route} from 'react-router-dom'
+import {withRouter} from 'react-router'
 import Register from './components/Auth/Register/Register.component';
 import Login from './components/Auth/Login/Login.component';
 import "./index.css"
+import firebase from './server/firebase'
 
+
+const Index=(props)=>{
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user)=>{
+      if(user){
+        props.history.push("/");
+      }else{
+        props.history.push("/login");
+      }
+    })
+  }, [])
+
+ 
+  return(
+    <Switch>
+        
+    <Route exact path="/register" component={Register}/>
+    <Route exact path="/login" component={Login}/>
+    <Route  path="/" component={App}/>
+    
+  </Switch>
+  )
+}
+
+const IndexWithRouter=withRouter(Index);
 
 
 ReactDOM.render(
   
     <Router>
-      <Switch>
-        
-        <Route exact path="/register" component={Register}/>
-        <Route exact path="/login" component={Login}/>
-        <Route  path="/" component={App}/>
-        
-      </Switch>
+     <IndexWithRouter/>
     </Router>
     
   ,
