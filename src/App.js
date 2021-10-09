56558a58836d61618a0dcdd239dbbf6748c95eaa
ScorchingShade <ankushors789@gmail.com>
2021-10-09 16:06:17 +0530

    added messages header, needs cleanup app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Sidebar from '../src/components/Sidebar/Sidebar.component.jsx'
 
 import './App.css';
+import Messages from './components/Messages/Messages.component.jsx';
 
 function App() {
   return (
     <div>
       <Sidebar/>
+      <div style={{paddingLeft:'350px'}}>
+      <Messages/>
+      </div>
+      
     </div>
   );
 }
